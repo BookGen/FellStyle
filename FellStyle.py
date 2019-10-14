e9afdb2ffac1d0056375eaccdf58839bd2c84c51
kibigo! <marrus-sh@users.noreply.github.com>
2019-10-14 00:02:41 -0700

    CSS updates for latest bookgen

--- FellStyle.py
+++ FellStyle.py
@@ -2,9 +2,6 @@
 
 from panflute import *
 from helper import *
-from datetime import datetime, timezone
-from collections import OrderedDict
-import re
 
 def defines(doc):
 	result = [RawInline('% FellStyle Metadata %', format='latex')]
@@ -24,11 +21,51 @@ def defines(doc):
 		])
 	return result
 
+def footer(doc):
+	result = [RawBlock('<footer id="FellStyle.footer">', format='html')]
+	result.extend(metadata.blocks(doc, 'FellStyle.publicationdetails'))
+	result.extend([
+		Para(
+			Str('Book design by '),
+			Link(Str('KIBI Gô'), url='https://go.KIBI.family/'),
+			Str('.')
+		),
+		Para(
+			Str('This work was generated with '),
+			Link(Str('FellStyle'), url='https://github.com/marrus-sh/FellStyle'),
+			Str('+'),
+			Link(Str('BookGen'), url='https://github.com/marrus-sh/BookGen'),
+			Str('.')
+		)
+	])
+	result.append(RawBlock('</footer>', format='html'))
+	return result
+
 def prepare(doc):
 	pass
 
 def action(elem, doc):
-	pass
+	if isinstance(elem, Header) and elem.level == 1 and doc.format in ['html', 'html5']:
+		header = []
+		header.extend(metadata.inlines(doc, 'series'))
+		value = metadata.inlines(doc, 'title')
+		if value:
+			if header:
+				header.append(Str(' – '))
+			href = metadata.text(doc, 'homepage', metadata.text(doc, 'index'))
+			if href:
+				header.append(Link(*value, url=href))
+			else:
+				header.extend(value)
+		if header:
+			header =[Plain(*header)]
+		header.append(elem)
+		value = metadata.inlines(doc, 'publisher')
+		if value:
+			header.append(Plain(*value))
+		header.insert(0, RawBlock('<header>', format='html'))
+		header.append(RawBlock('</header>', format='html'))
+		return header
 
 def finalize(doc):
 	if doc.format == 'latex':
@@ -41,6 +78,29 @@ def finalize(doc):
 			header_includes = MetaBlocks()
 		header_includes.content.append(Plain(*defines(doc)))
 		doc.metadata['header-includes'] = header_includes
+	elif doc.format in ['html', 'html5']:
+		value = metadata.text(doc, 'next')
+		if (value):
+			doc.content.append(Plain(Link(
+				*metadata.inlines(doc, 'FellStyle.localization-next', [Str('Next Chapter.')]),
+				url=value + '#BookGen.main' if value[0] == '.' else value,
+				identifier='FellStyle.main.next'
+			)))
+		doc.content = [
+			RawBlock('\n<!-- BEGIN BODY -->\n<article>', format='html'),
+			Div(*doc.content, identifier='FellStyle.main'),
+			RawBlock('</article>\n<!-- END BODY -->\n', format='html')
+		]
+		include_after = doc.get_metadata('include-after', MetaBlocks(), builtin=False)
+		if isinstance(include_after, MetaInlines):
+			include_after = MetaBlocks(Plain(*header_includes.content))
+		elif isinstance(include_after, MetaString):
+			include_after = MetaBlocks(Plain(Str(header_includes.text)))
+		elif not isinstance(include_after, MetaBlocks):
+			include_after = MetaBlocks()
+		include_after.content.extend(footer(doc))
+		doc.metadata['include-after'] = include_after
+
 
 def main(doc=None):
 	return run_filter(action, doc=doc, prepare=prepare, finalize=finalize)
